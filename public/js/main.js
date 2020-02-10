4e8622002ae7b7e6ba963e1e6ae18d487e0b7b3d
Gino Ghalehpardaz <ginoghalehpardaz@Ginos-MacBook-Pro-2.local>
2020-02-09 19:54:42 -0500

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("mainNav").style.width="375px";
+    document.getElementById("openBox").style.width="0px";
+}
+
+function closeNav(){
+    document.getElementById("mainNav").style.width="0px";
+    document.getElementById("openBox").style.width="0px";
+}
