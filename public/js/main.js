09c8cd3b3b80bb8726e68c7726750c0aa2435baa
Gino Ghalehpardaz <ginoghalehpardaz@Ginos-MacBook-Pro-2.local>
2020-06-19 20:02:35 -0400

    image slider

--- public/js/main.js
+++ public/js/main.js
@@ -8,3 +8,30 @@ var menu = document.getElementById('menu');
         closeIcon.checked = false;
       }
     }
+
+
+    var i = 0; //starting images
+    var images = [];
+    var time = 3000;
+
+
+    images[0] = 'public/images/boxingring.jpg';
+    images[1] = 'public/images/BasketballNetCinema4D.jpg';
+    images[2] = 'public/images/pokeball2.jpg';
+    images[3] = 'public/images/SoccerNet.jpg';
+    images[4] = 'public/images/Mug.jpg';
+    images[5] = 'public/images/CanFlag.jpg'
+
+    function slide(){
+      document.cinema4d.src = images[i];
+
+      if(i < images.length - 1){
+        i++;
+      } else {
+        i = 0;
+      }
+
+      setTimeout("slide()", time);
+    }
+
+    window.onload = slide;
